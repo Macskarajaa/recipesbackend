9ee81f05d37a69a415be2b13be9a612a2039dba3
Macskarajaa <farkastamas1337@gmail.com>
2025-11-30 19:08:00 +0100

    uhh idk

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import cloudinary from "./cloudinaryConfig.js"
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors({ origin:"https://recipesbackend-floe.vercel.app"}))
+
 app.use(express.json({limit:"5mb"}))
 
 //feltöltés végpont:
